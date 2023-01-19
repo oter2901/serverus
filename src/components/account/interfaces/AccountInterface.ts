f8dca360fefca7afedf94bf8e2a389025d7038ae
Oswaldo Tedesco <oter2901@gmail.com>
2023-01-19 08:40:02 +0000

    addapt models

--- src/components/account/interfaces/AccountInterface.ts
+++ src/components/account/interfaces/AccountInterface.ts
@@ -1,5 +1,49 @@
 export interface Account {
   id: number;
   email: string;
-  password: string;
+  password?: string;
+  external_type?: string;
+  external_id?: string;
+  status: string;
+  roles: string[];
+  sessions?: string[];
+
+  created_at: string;
+  updated_at?: string;
+  delete_at?: string;
 }
+
+// interface Account2 {
+//   id: string; //Required
+//   email: string;
+//   password: string;
+//   external_type: string; //relation with auth types tyble?
+//   external_id: string;
+//   status: enum; // status enum model
+//   roles: Model; // reference to roles table account may have one role for many profiles
+//   sessions: string; // reference to model session or ephemeral data on redis
+//   // timestamps
+//   created_at: Date;
+//   updated_at: Date;
+// }
+
+// interface account_role {
+//   id: string;
+//   account: Model; //reference to account table
+//   profiles: Model[]; // reference to profile table
+//   role: enum; //roles enum table for roles
+//   // timestamps
+//   created_at: Date;
+//   updated_at: Date;
+// }
+
+// ORGANIZATION AND END USERS PROFILE DATA
+// interface Profile {
+//   id: string;
+//   name: string;
+//   type: enum; // profile type enum table
+//   last_name?: string;
+//   avatar_url: string;
+//   created_at: Date;
+//   updated_at: Date;
+// }
--- src/components/account/models/AccountModel.ts
+++ src/components/account/models/AccountModel.ts
@@ -5,9 +5,24 @@ import { Account } from '../interfaces/AccountInterface';
 
 export class Accounts extends Model implements Account {
   id!: number;
-  email!: string;
-  password!: string;
+  email: string;
+  password: string;
+  external_type?: string;
+  external_id?: string;
+  status: string;
+  roles: string[];
+  sessions: string[];
 
+  created_at!: string;
+  updated_at?: string;
+  delete_at?: string;
+  $beforeInsert() {
+    this.created_at = new Date().toISOString();
+  }
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
   static tableName = ACCOUNT_TABLE; // database table name
   static idColumn = 'id'; // id column name
 }
